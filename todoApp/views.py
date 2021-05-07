6bb73c121eb550e8334bf8cdce615a35e4537f87
Vinit Rathod <vinitrathod100901@gmail.com>
2021-05-07 21:58:51 +0530

    first commit

---
+++ todoApp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from .models import TodoListItem
+from django.views import generic
+
+# Create your views here.
+"""def index(request):
+    all_todo_items = TodoListItem.objects.all()
+    context = {
+        'all_items':all_todo_items
+    }
+    return render(request,'todoApp/todoList.html',context)
+    #return HttpResponse('Hello from TodoApp')"""
+
+class IndexView(generic.ListView):
+    template_name = 'todoApp/todoList.html'
+    #context_object_name is by default object_list
+    #for changing it do the below code
+    context_object_name = 'all_items'
+    
+    def get_queryset(self):
+        return TodoListItem.objects.all()
+
+def addtoview(request):
+    x = request.POST['content']
+    new_item = TodoListItem(content = x)
+    new_item.save()
+    return HttpResponseRedirect('/')
+
+def deleteTodoView(request,i):
+    y = TodoListItem.objects.get(id = i)
+    y.delete()
+    return HttpResponseRedirect('/')
+
