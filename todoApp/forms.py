998cb9ddc68fddcf301bce8d08cab187884ae5e7
Vinit Rathod <vinitrathod100901@gmail.com>
2021-05-15 13:27:20 +0530

    Added New Form UserCreationForm for Registration

--- todoApp/forms.py
+++ todoApp/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForm(forms.Form):
     first_name = forms.CharField(max_length = 20)
     last_name = forms.CharField(max_length = 20)
     email = forms.EmailField(max_length = 50)
     message = forms.CharField(widget = forms.Textarea, max_length = 2000)
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
