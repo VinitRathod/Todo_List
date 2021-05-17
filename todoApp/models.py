0236ea79f7895658d429ab3733b3fb3eb6c2e371
Vinit Rathod <vinitrathod100901@gmail.com>
2021-05-17 20:49:03 +0530

    Added time and done models

--- todoApp/models.py
+++ todoApp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class TodoListItem(models.Model):
     content = models.TextField()
-    time = models.TimeField(auto_now=True)
+    time = models.DateTimeField(default=timezone.now())
     done = models.BooleanField(default=False)
