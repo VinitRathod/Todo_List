ca29f1d7f09b789be72efd19a23725c31ae8cd8b
Vinit Rathod <vinitrathod100901@gmail.com>
2021-05-17 15:30:23 +0530

    Added two new fields time and done.

--- todoApp/models.py
+++ todoApp/models.py
@@ -3,3 +3,5 @@ from django.db import models
 # Create your models here.
 class TodoListItem(models.Model):
     content = models.TextField()
+    time = models.TimeField(auto_now=True)
+    done = models.BooleanField(default=False)
